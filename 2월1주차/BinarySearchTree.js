371b1e0b6f9c9bb686a58fa67e4cb81968698c73
YoungHo Kwak <jason.uphiller@gmail.com>
2019-02-01 22:54:06 +0900

    Update BinarySearchTree.js

--- 2월1주차/BinarySearchTree.js
+++ 2월1주차/BinarySearchTree.js
@@ -1,33 +1,39 @@
 
 //https://www.acmicpc.net/problem/1539
 
-let P =[9,1,4,3,2,5,6,7,8,0]
+var fs = require('fs');
+var input = fs.readFileSync('/dev/stdin').toString().split('\n');
+var size = parseInt(input[0]);
+var array = [];
+for(var i = 1 ; i <= size ; i++){
+    array.push(parseInt(input[i]))
+}
 
-    const node = {value:9, left:null, right:null};
-    let sumHeight = 0;
+var node = {value:array[0], left:null, right:null};
+var sumHeight = 0;
 
-    const insert = (root, element, height) => {
-      if(root.value > element){
-        if (root.left) {
-          insert( root.left, element, height+1);
-        } else {
-          const leftNode = {value:element, left:null, right:null};
-          root.left = leftNode;
-          sumHeight += height;
-        }
-      }else{
-        if (root.right) {
-          insert( root.right, element, height+1);
-        } else {
-          const rightNode = {value:element, left:null, right:null};
-          root.right = rightNode;
-          sumHeight += height;
-        }
-      }
-   };
-
-    for (let i=1; i < 10 ; i++) {
-      insert(node, P[i], 2);
+function insert(root, element, height){
+  if(root.value > element){
+    if (root.left) {
+      insert( root.left, element, height+1);
+    } else {
+      var leftNode = {value:element, left:null, right:null};
+      root.left = leftNode;
+      sumHeight += height;
     }
+  }else{
+    if (root.right) {
+      insert( root.right, element, height+1);
+    } else {
+      var rightNode = {value:element, left:null, right:null};
+      root.right = rightNode;
+      sumHeight += height;
+    }
+  }
+};
 
-    console.log(sumHeight+1);
+for (var i=1; i < 10 ; i++) {
+  insert(node, array[i], 2);
+}
+
+console.log(sumHeight+1);
