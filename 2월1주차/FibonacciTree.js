bcedfdb8622c5256ecbe5662d40f79d0f6e08dad
YoungHo Kwak <jason.uphiller@gmail.com>
2019-02-21 19:10:33 +0900

    Update FibonacciTree.js

--- 2월1주차/FibonacciTree.js
+++ 2월1주차/FibonacciTree.js
@@ -16,3 +16,27 @@ function fibonacci(n, node){
       return (fib(n-1, rightNode)+fib(n-2, leftNode));
     }
 } 
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main{
+  public static int i, n, h[];
+  public static ArrayList<Integer> list = new ArrayList<>();
+  public static void main(String[] args) throws Exception{
+    Scanner sc = new Scanner(System.in);
+    n = sc.nextInt();
+    for(int i = 0 ; i <= n ; i++){
+        System.out.println(fibo(i));
+    }
+   
+    sc.close();
+  }
+  
+  public static int fibo(int n) {
+    if (n <= 1)
+        return n;
+    else 
+        return fibo(n-2) + fibo(n-1);
+  }
+}
